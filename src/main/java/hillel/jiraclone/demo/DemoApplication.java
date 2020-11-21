a0a006c65ed20969ae9ac5881f4c320ce98079b4
ktvcv <kotovec.vii@gmail.com>
2020-11-21 17:17:57 +0200

    add equals and hashcodes and fixed task reference

--- src/main/java/hillel/jiraclone/demo/DemoApplication.java
+++ src/main/java/hillel/jiraclone/demo/DemoApplication.java
@@ -12,31 +12,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/default_beans.xml");
 
-        UserService userService = applicationContext.getBean(UserService.class);
-
-        ProjectService projectService = applicationContext.getBean(ProjectService.class);
-
-        BacklogService backlogService = applicationContext.getBean(BacklogService.class);
-
-        SprintService sprintService = applicationContext.getBean(SprintService.class);
-
-        TaskService taskService = applicationContext.getBean(TaskService.class);
-
-        CommentService commentService = applicationContext.getBean(CommentService.class);
-
-        User user = new User();
-
-        user.setEmail("email");
-        user.setName("name");
-        user.setPassword("password");
-        userService.saveOrUpdate(user);
-
-        System.out.println(userService.getAll());
-
-        Project project = new Project();
-        User userFromDB = userService.getUserByEmail("email");
-        userFromDB.getProjects().add(project);
-
     }
 
 }
